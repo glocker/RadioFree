e53e6824769e4403b2dc2ff5604ce728a6bec3ef
George Glocker <georgeglocker@gmail.com>
2022-07-02 21:44:41 +0700

    Finished rendering colors from URL hash
    The main part of assignment completed

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,33 @@
 import React from 'react';
 
 const Main = () => {
+
+    // Get hash from URL address
+    const hash = window.location.href;
+
+    // Write down the regex which helps us get colors
+    const regex = /http:\/\/localhost:8080\/#tags=([a-zA-Z]+(,[a-zA-Z]+)+)/i;
+
+    // Get the array of colors: strings
+    const colorsList = regex.exec(hash)[1].split(',');
+
+    function renderColors(colorsList) {
+
+        // For every color in array return <li> element
+        return colorsList.map((color, index) => {
+            return <li key={index}>
+                        {color}
+                   </li>
+        });
+    }
+
     return (
-        <h3>Radio Liberty</h3>
+        <div>
+            <h3>Radio Liberty</h3>
+            <ul>
+                {renderColors(colorsList)}
+            </ul>
+        </div>
     );
 };
 
